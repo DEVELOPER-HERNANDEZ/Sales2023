40e0389ce3f01edd99be08eceff1dc89d2b96e0f
Hernandez Developer <hernandezrosarioelvis@gmail.com>
2024-03-02 14:51:51 -0800

    Complete countries API controller

---
+++ Sales.WEB/Repositories/HttpReponseWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace Sales.WEB.Repositories
+{
+    public class HttpReponseWrapper<T>
+    {
+        public HttpReponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+
+        public bool Error { get; set; }
+        public T? Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string> GetErrorMessage()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            else if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que logearte para hacer esta operacion";
+            }
+            else if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para hacer esta operacion";
+            }
+
+            return "Ha ocurrido un error inesperado";
+
+        }
+
+    }
+}
---
+++ Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpReponseWrapper<T>> Get<T>(string url);
+        Task<HttpReponseWrapper<object>> Post<T>(string url,T mode);
+        Task<HttpReponseWrapper<TReponse>> Post<T,TReponse>(string url,T mode);
+
+    }
+}
